9ffcb89c4c4eef4c43d75e0b5e1dffe9adce3e36
AlexDinh-coder <dttrung2703@gmail.com>
2025-08-11 00:09:33 +0700

    model resume

--- src/main/java/vn/tuantrung/jobhunter/domain/Resume.java
+++ src/main/java/vn/tuantrung/jobhunter/domain/Resume.java
@@ -1,70 +1,55 @@
 package vn.tuantrung.jobhunter.domain;
 
 import java.time.Instant;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Column;
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.tuantrung.jobhunter.util.SecurityUtil;
-import vn.tuantrung.jobhunter.util.constant.GenderEnum;
+import vn.tuantrung.jobhunter.util.constant.ResumeStateEnum;
 
 @Entity
-@Table(name = "users")
+@Table(name = "resumes")
 @Getter
 @Setter
-public class User {
+public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String name;
-
-    @NotBlank(message = "email is not empty")
     private String email;
 
-    @NotBlank(message = "password is not empty")
-    private String password;
-
-    private int age;
+    private String url;
 
     @Enumerated(EnumType.STRING)
-    private GenderEnum gender;
+    private ResumeStateEnum status;
 
-    private String address;
-
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String refreshToken;
     private Instant createdAt;
     private Instant updatedAt;
+
     private String createdBy;
     private String updatedBy;
 
     @ManyToOne
-    @JoinColumn(name = "company_id")
-    private Company company;
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    @JsonIgnore
-    List<Resume> resumes;
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
 
-    @PrePersist
+     @PrePersist
     public void handleBeforeCreate() {
         this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
                 ? SecurityUtil.getCurrentUserLogin().get()
@@ -80,5 +65,6 @@ public class User {
                 : "";
         this.updatedAt = Instant.now();
     }
+
 
 }
---
+++ src/main/java/vn/tuantrung/jobhunter/util/constant/ResumeStateEnum.java
@@ -0,0 +1,6 @@
+package vn.tuantrung.jobhunter.util.constant;
+
+public enum ResumeStateEnum {
+    PENDING, REVIEWING, APPROVED, REJECTED;
+    
+}
