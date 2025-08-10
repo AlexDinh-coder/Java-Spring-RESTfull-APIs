cc6a705908b9e68ba1600ef03c2ee4ff8827a982
AlexDinh-coder <dttrung2703@gmail.com>
2025-08-10 23:57:26 +0700

    Donwload file

--- src/main/java/vn/tuantrung/jobhunter/controller/FileController.java
+++ src/main/java/vn/tuantrung/jobhunter/controller/FileController.java
@@ -8,6 +8,7 @@ import vn.tuantrung.jobhunter.service.FileService;
 import vn.tuantrung.jobhunter.util.annotation.ApiMessage;
 import vn.tuantrung.jobhunter.util.error.StorageException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -15,11 +16,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,6 +35,7 @@ public class FileController {
     private String baseUri;
 
     private final FileService fileService;
+
     public FileController(FileService fileService) {
         this.fileService = fileService;
     }
@@ -37,10 +44,9 @@ public class FileController {
     @ApiMessage("Upload single file to server")
     public ResponseEntity<ResUploadFileDTO> upload(@RequestParam(name = "file", required = false) MultipartFile file,
             @RequestParam("folder") String folder) throws URISyntaxException, IOException, StorageException {
-        
 
-        //skip validate
-        if(file == null || file.isEmpty()){
+        // skip validate
+        if (file == null || file.isEmpty()) {
             throw new StorageException("File is empty. Please upload a file");
         }
         String fileName = file.getOriginalFilename();
@@ -49,17 +55,42 @@ public class FileController {
 
         if (!isValid) {
             throw new StorageException("Invalid file extension. Only allows" + allowedExtensions.toString());
-            
+
         }
-        //create a directory if not exist
+        // create a directory if not exist
         this.fileService.createDirectory(baseUri + folder);
 
-        //store file
+        // store file
         String uploadFile = this.fileService.store(file, folder);
         ResUploadFileDTO res = new ResUploadFileDTO(uploadFile, Instant.now());
 
-
         return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/files")
+    @ApiMessage("Download a file ")
+    public ResponseEntity<Resource> donwloadFile(@RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "folder", required = false) String folder)
+            throws StorageException, URISyntaxException, FileNotFoundException {
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing required params: (fileName or folder ) in query params.");
+        }
+
+        // check file exist ( and not a directory)
+        long fileLength = this.fileService.getFileLength(fileName, folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + " not found. ");
+        }
+
+        // download a file
+        InputStreamResource resource = this.fileService.getResource(fileName, folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+        
     }
 
 }
--- src/main/java/vn/tuantrung/jobhunter/service/FileService.java
+++ src/main/java/vn/tuantrung/jobhunter/service/FileService.java
@@ -1,6 +1,8 @@
 package vn.tuantrung.jobhunter.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,12 +13,12 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileService {
-
 
     @Value("${tuantrung.upload-file.base-uri}")
     private String baseUri;
@@ -39,7 +41,7 @@ public class FileService {
     }
 
     public String store(MultipartFile file, String folder) throws URISyntaxException, IOException {
-       // create unique filename
+        // create unique filename
         String finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
 
         URI uri = new URI(baseUri + folder + "/" + finalName);
@@ -51,4 +53,25 @@ public class FileService {
         return finalName;
     }
 
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(baseUri + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File tmpDir = new File(path.toString());
+
+        // file not exists, or file is 1 directory => return 0;
+        if (!tmpDir.exists() || tmpDir.isDirectory())
+            return 0;
+        return tmpDir.length();
+
+    }
+
+    public InputStreamResource getResource(String fileName, String folder)
+            throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(baseUri + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
+    }
 }
