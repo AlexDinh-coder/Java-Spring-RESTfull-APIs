59ce5a6f9d509c8042551573cb8c1ca221aa1c5f
AlexDinh-coder <dttrung2703@gmail.com>
2025-08-12 00:07:58 +0700

    update api user and role

--- src/main/java/vn/tuantrung/jobhunter/service/UserService.java
+++ src/main/java/vn/tuantrung/jobhunter/service/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import vn.tuantrung.jobhunter.domain.Company;
+import vn.tuantrung.jobhunter.domain.Role;
 import vn.tuantrung.jobhunter.domain.User;
 import vn.tuantrung.jobhunter.domain.response.ResCreateUserDTO;
 import vn.tuantrung.jobhunter.domain.response.ResUpdateUserDTO;
@@ -24,9 +25,12 @@ public class UserService {
 
     private final CompanyService companyService;
 
-    public UserService(UserRepository userRepository, CompanyService companyService) {
+    private final RoleService roleService;
+
+    public UserService(UserRepository userRepository, CompanyService companyService, RoleService roleService) {
         this.userRepository = userRepository;
         this.companyService = companyService;
+        this.roleService = roleService;
     }
 
     public User handleCreateUser(User user) {
@@ -36,6 +40,13 @@ public class UserService {
             user.setCompany(companyOptional.isPresent() ? companyOptional.get() : null);
 
         }
+
+        //check role
+        if(user.getRole() != null){
+            Role r = this.roleService.fetchById(user.getRole().getId());
+            user.setRole(r != null ? r : null);
+        }
+
         return this.userRepository.save(user);
     }
 
@@ -80,11 +91,18 @@ public class UserService {
     public ResUserDTO convertToResUserDTO(User user) {
         ResUserDTO res = new ResUserDTO();
         ResUserDTO.CompanyUser com = new ResUserDTO.CompanyUser();
+        ResUserDTO.RoleUser roleUser = new ResUserDTO.RoleUser();
 
         if (user.getCompany() != null) {
             com.setId(user.getCompany().getId());
             com.setName(user.getCompany().getName());
             res.setCompany(com);
+        }
+
+        if (user.getRole() != null) {
+            roleUser.setId(user.getRole().getId());
+            roleUser.setName(user.getRole().getName());
+            res.setRole(roleUser);
         }
         res.setId(user.getId());
         res.setEmail(user.getEmail());
@@ -113,19 +131,10 @@ public class UserService {
         rs.setMeta(meta);
 
         // remove sensitive data
-        List<ResUserDTO> listUser = pageUser.getContent()
-                .stream().map(item -> new ResUserDTO(
-                        item.getId(),
-                        item.getName(),
-                        item.getEmail(),
-                        item.getGender(),
-                        item.getAddress(),
-                        item.getAge(),
-                        item.getCreatedAt(),
-                        item.getUpdatedAt(),
-                        new ResUserDTO.CompanyUser(item.getCompany() != null ? item.getCompany().getId() : 0,
-                                item.getCompany() != null ? item.getCompany().getName() : null)))
+        List<ResUserDTO> listUser = pageUser.getContent().stream()
+                .map(item -> this.convertToResUserDTO(item))
                 .collect(Collectors.toList());
+                
         rs.setResult(listUser);
         return rs;
     }
@@ -159,10 +168,14 @@ public class UserService {
             // check company
             if (reqUser.getCompany() != null) {
 
-                // Optional<Company> companyOptional = this.companyService.findById(reqUser.getCompany().getId());
-                // reqUser.setCompany(companyOptional.isPresent() ? companyOptional.get() : null);
                 Optional<Company> companyOptional = this.companyService.findById(reqUser.getCompany().getId());
                 currentUser.setCompany(companyOptional.isPresent() ? companyOptional.get() : null);
+            }
+
+            //check role
+            if(reqUser.getRole() != null) {
+                Role r = this.roleService.fetchById(reqUser.getRole().getId());
+                currentUser.setRole(r != null ? r : null);
             }
 
             // update
